52d870c1f0a3007d4de33a6f7eb9ae6c7252aabe
ywan3 <yajie_wan@brown.edu>
2024-03-20 17:10:27 -0400

    wallpaper background

--- Bounce/src/BallPanel.java
+++ Bounce/src/BallPanel.java
@@ -2,14 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 class BallPanel extends JPanel {
     private ArrayList<Ball> balls = new ArrayList<>();
     private final int BALL_COUNT = 10;
+    private Image backgroundImage;
 
     public BallPanel() {
-        setBackground(new Color(173, 216, 230)); 
+        try {
+            backgroundImage = ImageIO.read(new File("./wallpaper.jpg"));
+            System.out.println(backgroundImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < BALL_COUNT; i++) {
             Ball ball = new Ball();
             balls.add(ball);
@@ -20,10 +29,13 @@ class BallPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); 
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+
         for (Ball ball : balls) {
             ball.draw(g);
         }
     }
 }
-
